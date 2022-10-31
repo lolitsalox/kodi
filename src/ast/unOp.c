0c90f66a23ca1e86c6d7780e94c6c2044fff7195
lolitsalox <drdy.business@gmail.com>
2022-11-01 00:34:05 +0200

    Implemented print

--- src/ast/unOp.c
+++ src/ast/unOp.c
@@ -3,9 +3,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+static void Print(astUnOp_t* self, size_t indent) {
+    INDENT(indent)
+    printf("%s - %s\n", astTypeToStr(self->base.base.type), tokenTypeToStr(self->base.op));
+
+    INDENT(indent + 1)
+    printf("first:\n");
+    self->first->Print(self->first, indent + 2);
+}
+
 void AstUnOp(astUnOp_t* self, tokenType_t op, ast_t* first) {
     AstOp(&self->base, op);
     self->base.base.type = AST_UNARY_OP;
+    self->base.base.Print = (void (*)(ast_t*, size_t)) Print;
 
     self->first = first;
 }

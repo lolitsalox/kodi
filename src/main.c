924e4a76cfa180f2926f21734fb8a2db76f7829f
lolitsalox <drdy.business@gmail.com>
2022-10-31 20:50:45 +0200

    Main function

--- src/main.c
+++ src/main.c
@@ -1,7 +1,20 @@
+#include "gen_bytekod.h"
+
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char** argv) {
+    
+    ByteCode* bkod = gen_bytekod("main.kod");
+    if (!bkod) return 1;
+    
+    for (size_t i = 0; i < bkod->size; ++i) {
+        printf("%02X ", bkod->code[i]);
+    }
 
-    printf("Hello world!\n");
+    free(bkod->code);
+    free(bkod);
+
+    // run_bytekod();
     return 0;
 }
